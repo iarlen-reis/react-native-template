eeaeb4eed134ae0dced8d7c2616afc32a3d4669c
Iarlen <iarlensr@hotmail.com>
2023-05-31 15:10:52 -0300

    chore: update some libs.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
 
 const Layout = () => {
   return (
-    <>
+    <View className="flex-1 bg-gray-800">
       <StatusBar style="light" />
       <Stack
         screenOptions={{
@@ -13,7 +14,7 @@ const Layout = () => {
       >
         <Stack.Screen name="index" />
       </Stack>
-    </>
+    </View>
   )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 const index = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-lg">Template React Native</Text>
+      <Text className="text-lg text-white">Template React Native</Text>
     </View>
   )
 }
